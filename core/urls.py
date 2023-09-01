ff0caff96c3da06bb04d980339bb6485793fbd57
Ersin <ersinmandaci0@gmail.com>
2023-09-01 19:33:43 +0300

    refactor: create includes

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from .views import index, contact
 
 urlpatterns = [
     path('', index ,name='index'),
-    path('contact', contact, name='index'),
+    path('contact', contact, name='contact'),
 ]
